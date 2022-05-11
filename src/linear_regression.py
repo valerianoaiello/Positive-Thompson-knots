01c3f41f9892df8232775d20bbf9c936d6d4efe9
valerianoaiello <104353469+valerianoaiello@users.noreply.github.com>
2022-05-11 17:23:17 +0200

    Small changes to linear_regression

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -1,6 +1,6 @@
 #https://stackoverflow.com/questions/35491274/split-a-pandas-column-of-lists-into-multiple-columns
 #RENDERE VARIABILE m GLOBALE, IN TUTTI I FILE
-
+import tree
 from unicodedata import name
 import pandas as pd
 import numpy as np
@@ -9,6 +9,7 @@ from scipy.stats import skew
 from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+from sympy import variations
 plt.style.use("ggplot")
 plt.rcParams['figure.figsize'] = (12, 8)
 
@@ -42,9 +43,12 @@ table2.to_csv('data_bis.csv', index=False)
 #table.info()
 
 #non lo stampa :-(
-#sns.pairplot(table2,x_vars=["leaves"], y_vars="leaves", height=10, aspect=.7)
+sns.pairplot(table2,x_vars=['leaves', 'x0', 'x1','x2','x3','x4','x5'], y_vars="orbits", height=10, aspect=.7)
+
+
+#plt.show()
 #sns.pairplot(table,x_vars=['leaves', 'x0', 'x1','x2','x3','x4','x5'], y_vars='orbits', height=10, aspect=.7)
-#plt.savefig(fname='prova.jpg')
+plt.savefig(fname='prova.jpg')
 
 X=table2.loc[:,['leaves','x0', 'x1','x2','x3','x4','x5','x6']]
 y=table2['orbits']
@@ -55,4 +59,14 @@ print(lm1.coef_)
 
 w=zip(['leaves','x0', 'x1','x2','x3','x4','x5','x6'], lm1.coef_)
 list(w)
-print(w)
+print(type(lm1.coef_))
+print(lm1.coef_.shape)
+vv=np.array([1, 2, 3,4,5,6,7])
+n=tree.number_leaves_ternary(vv)
+vector=np.array([n,1, 2, 3,4,5,6,7])
+#input('Insert the exponents of x0, x1 x2 x3 x4 x5 x6')
+number_pred = np.dot(lm1.coef_,vector)
+print('prediction:', number_pred)
+print('real value:', len(tree.whole_permutation(tree.number_leaves_ternary(vector),vector)))
+
+lm1_preds=lm1.predict(X[['leaves','x0', 'x1','x2','x3','x4','x5','x6']])
