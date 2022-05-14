38310327fa9fc1fbda4a2e3b763b633a5f22a66b
Stefano Iovieno <stefano.iovieno@gmail.com>
2022-05-14 16:07:58 +0200

    Add utilities for linear regression

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -1,5 +1,7 @@
 #https://stackoverflow.com/questions/35491274/split-a-pandas-column-of-lists-into-multiple-columns
 #RENDERE VARIABILE m GLOBALE, IN TUTTI I FILE
+from typing import List
+from xmlrpc.client import Boolean
 from constants import WIDTH_VECTOR
 import tree
 from unicodedata import name
@@ -7,8 +9,9 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from scipy.stats import skew
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sympy import variations
 plt.style.use("ggplot")
@@ -19,58 +22,29 @@ print(table.head())
 
 m=WIDTH_VECTOR 
 exponents = []
-  
+
+def split_dataset_train_test(X: pd.DataFrame, y: pd.DataFrame, test_size: float, shuffle: Boolean=True) -> List[pd.DataFrame]:
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=shuffle)
+    return X_train, X_test, y_train, y_test
+
+def mse(y_test: pd.DataFrame, y_pred: pd.DataFrame) -> float:
+    return mean_squared_error(y_test, y_pred)
+
 X=table.loc[:,['leaves', *[ 'x'+str(i) for i in range(m)]]]
 y=table['orbits']
+
+X_train, X_test, y_train, y_test = split_dataset_train_test(X, y, 0.3)
+
 lm1=LinearRegression()
-lm1.fit(X,y)
+lm1.fit(X_train,y_train)
 print(lm1.intercept_)
 print(lm1.coef_)
 
-vv=np.array([1, 2, 3,4])
-n=tree.number_leaves_ternary(vv)
-vector=np.array([n,1, 2, 3,4])
-number_pred = np.dot(lm1.coef_,vector)
-print('prediction:', number_pred)
-print('real value:', len(tree.whole_permutation(tree.number_leaves_ternary(vector),vector)))
+y_pred = lm1.predict(X_test)
 
-exit(0)
+error: float = mse(y_test, y_pred)
 
-sns.pairplot(table,x_vars=['leaves', 'x0', 'x1','x2','x3','x4','x5'], y_vars="orbits", height=10, aspect=.7)
-plt.savefig(fname='prova.jpg')
+print("ERROR = ", error)
 
-y=table['orbits']
-lm1=LinearRegression()
-lm1.fit(X,y)
-print(lm1.intercept_)
-print(lm1.coef_)
-
-#tablecomponents = sns.load_dataset("table")
-plt.plot(table[leaves], table['x0'])
-plt.savefig(fname='prova.jpg')
-
-
-#plt.show()
-#sns.pairplot(table,x_vars=['leaves', 'x0', 'x1','x2','x3','x4','x5'], y_vars='orbits', height=10, aspect=.7)
-plt.savefig(fname='prova.jpg')
-
-X=table2.loc[:,['leaves','x0', 'x1','x2','x3','x4','x5','x6']]
-y=table2['orbits']
-lm1=LinearRegression()
-lm1.fit(X,y)
-print(lm1.intercept_)
-print(lm1.coef_)
-
-w=zip(['leaves','x0', 'x1','x2','x3','x4','x5','x6'], lm1.coef_)
-list(w)
-print(type(lm1.coef_))
-print(lm1.coef_.shape)
-vv=np.array([1, 2, 3,4,5,6,7])
-n=tree.number_leaves_ternary(vv)
-vector=np.array([n,1, 2, 3,4,5,6,7])
-#input('Insert the exponents of x0, x1 x2 x3 x4 x5 x6')
-number_pred = np.dot(lm1.coef_,vector)
-print('prediction:', number_pred)
-print('real value:', len(tree.whole_permutation(tree.number_leaves_ternary(vector),vector)))
-
-lm1_preds=lm1.predict(X[['leaves','x0', 'x1','x2','x3','x4','x5','x6']])
+sns.residplot(x=y_test, y=y_pred)
+plt.show()
