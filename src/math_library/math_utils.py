830b4977c3072da1ff5bb9b29f9f4de1dadef74d
Stefano Iovieno <stefano.iovieno@gmail.com>
2022-11-06 11:27:19 +0100

    Formatting math_utils.py

--- src/math_library/math_utils.py
+++ src/math_library/math_utils.py
@@ -1,12 +1,15 @@
 import numpy as np
+
 from sympy.combinatorics import Permutation
 
-#Go to https://docs.sympy.org/latest/modules/combinatorics/permutations.html
-#for more information on Permutation
+# Go to https://docs.sympy.org/latest/modules/combinatorics/permutations.html
+# for more information on Permutation
 """
 v = (v_1, v_2, ..., v_n)
 0 <= v_i < m, v_i in N, for every i
 """
+
+
 class MathUtils:
     def __init__(self) -> None:
         pass
@@ -17,36 +20,37 @@ class MathUtils:
     """
 
     def bottom_permutation(self, n: int) -> np.ndarray:
-        if n%2 == 0:
+        if n % 2 == 0:
             print('The number is even')
             exit(0)
         if n == 1:
-            p=Permutation(0, 1)
+            p = Permutation(0, 1)
         elif n == 3:
             p = Permutation(0, 2)(1, 3)
         else:
             q = Permutation(0, 2)
             m = int((n-3)/2)
             for i in range(m):
-                q = Permutation(q(2*i+1,2*i+4))
+                q = Permutation(q(2*i+1, 2*i+4))
             p = Permutation(q(n-2, n))
-        
+
         return np.array(p.array_form)
 
     """
     This function computes the permutation associated with the top tree of a positive Thompson element
     n is odd number and the permutation acts on {0,1,...,n}
     """
+
     def top_permutation(self, n: int, v: np.ndarray) -> np.ndarray:
-        if n%2==0:
+        if n % 2 == 0:
             print('The number is even')
             exit(0)
-        if n==1:
-            p=np.array([1,0])
-        elif n==3:
-            p=np.array([2,3,0,1])
-        elif n>=5 and not np.any(v):
-            p=self.bottom_permutation(n)
+        if n == 1:
+            p = np.array([1, 0])
+        elif n == 3:
+            p = np.array([2, 3, 0, 1])
+        elif n >= 5 and not np.any(v):
+            p = self.bottom_permutation(n)
         else:
             z = np.nonzero(v)[0]
             k = len(z)
@@ -67,7 +71,7 @@ class MathUtils:
                 elif i >= a + 2 and w[i] >= a + 2:
                     p[i + 2] = w[i] + 2
                 elif i >= a + 2 and w[i] <= a:
-                    p[i + 2] = w[i]                
+                    p[i + 2] = w[i]
                 elif i >= a + 2 and w[i] == a + 1:
                     p[i + 2] = a + 2
                 elif i == a + 1 and w[i] >= a + 2:
@@ -78,18 +82,17 @@ class MathUtils:
             p[a + 3] = a + 1
         return p
 
-
     def whole_permutation(self, n: int, v: np.ndarray) -> np.ndarray:
         p = self.top_permutation(n, v)
-        q = self.bottom_permutation(n) 
-        
+        q = self.bottom_permutation(n)
+
         tot = []
-        
+
         for i in range(n):
             l: list = []
             if i not in [item for sublist in tot for item in sublist]:
                 while True:
-                    if p[i] not in l:       
+                    if p[i] not in l:
                         l.append(p[i])
                         i = p[i]
                         if q[i] not in l:
@@ -102,28 +105,28 @@ class MathUtils:
                 tot.append(l)
         return tot
 
+    # this function find the number of leaves in the reduced binary tree representing an element in F_+
 
-    #this function find the number of leaves in the reduced binary tree representing an element in F_+
     def number_leaves_binary(self, v: np.ndarray) -> int:
         n = (self.number_leaves_ternary(v) + 1)/2
         return n
 
-    #this function find the number of leaves in the reduced ternary tree representing an element in F_{3,+}
+    # this function find the number of leaves in the reduced ternary tree representing an element in F_{3,+}
     def number_leaves_ternary(self, v: np.ndarray) -> int:
         z = np.nonzero(v)[0]
-        k = len(z)   
+        k = len(z)
         m = 1000
         w = np.array(list(range(m)))
         u = w.copy()
-        for i in range(k-1,-1,-1):
+        for i in range(k-1, -1, -1):
             for j in range(v[z[i]]):
-                w = np.delete(w,z[i]+1)
-                w = np.delete(w,z[i]+1)
+                w = np.delete(w, z[i]+1)
+                w = np.delete(w, z[i]+1)
         n = 0
         for i in range(len(w)-1):
             if w[i + 1] - w[i] >= 2:
                 n = w[i + 1]
-        if n%2 == 1:
+        if n % 2 == 1:
             n = n + 2
         else:
             n = n + 1
@@ -139,6 +142,8 @@ class MathUtils:
         return w
 
     def random_generate_vectors(self, vector_number: int, vector_height: int, vector_width: int) -> np.ndarray:
-        vector_matrix: np.ndarray = np.random.randint(vector_height, size=(vector_number, vector_width))
-        vector_matrix = vector_matrix[(vector_matrix[:, 0] != 0) | (vector_matrix[:, 1] != 0)]
+        vector_matrix: np.ndarray = np.random.randint(
+            vector_height, size=(vector_number, vector_width))
+        vector_matrix = vector_matrix[(
+            vector_matrix[:, 0] != 0) | (vector_matrix[:, 1] != 0)]
         return vector_matrix
