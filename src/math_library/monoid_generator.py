4ad24547d65853e250418d6e9e9d9ccd2dc2ffc6
Stefano Iovieno <stefano.iovieno@gmail.com>
2022-11-26 16:29:23 +0100

    Change naming conventions in MonoidElementsGenerator

--- src/math_library/monoid_generator.py
+++ src/math_library/monoid_generator.py
@@ -1,25 +1,26 @@
 import numpy as np
 
-class MonoidGenerator:
+
+class MonoidElementGenerator:
     def __init__(self, height: int, width: int) -> None:
         self.height = height
         self.width = width
 
     def generate_monoid_elements(self) -> np.ndarray:
-        v = list(range(self.height))
-        l = [v for i in range(self.width)]
+        admissible_exponents = list(range(self.height))
 
-        w = np.array(np.meshgrid(*l)).T.reshape(-1, self.width)
-        w = w[(w[:, 0] != 0) | (w[:, 1] != 0)]
+        repeated_admissible_exponents = [admissible_exponents]*self.width
 
-        return w
+        elements_matrix = np.array(np.meshgrid(*repeated_admissible_exponents)).T.reshape(-1, self.width)
 
-    def random_generate_monoid_elements(self, vector_number: int) -> np.ndarray:
-        vector_matrix: np.ndarray = np.random.randint(
-            self.height, size=(vector_number, self.width))
-        vector_matrix = vector_matrix[(
-            vector_matrix[:, 0] != 0) | (vector_matrix[:, 1] != 0)]
-        return vector_matrix
+        return elements_matrix
+
+    def random_generate_monoid_elements(self, number_of_elements: int) -> np.ndarray:
+        elements_matrix: np.ndarray = np.random.randint(
+            self.height, size=(number_of_elements, self.width))
+
+        return elements_matrix
+
 
 if __name__ == '__main__':
-    print(MonoidGenerator.generate_monoid_elements())
+    print(MonoidElementGenerator(height=5, width=10).generate_monoid_elements())
