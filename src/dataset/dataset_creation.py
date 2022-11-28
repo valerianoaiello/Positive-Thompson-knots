b5c7af5969dec038970938d0595d2232c0fd8ad9
Stefano Iovieno <stefano.iovieno@gmail.com>
2022-11-29 00:37:28 +0100

    Update dataset_creation.csv

--- src/dataset/dataset_creation.py
+++ src/dataset/dataset_creation.py
@@ -5,29 +5,53 @@ from sympy.combinatorics import Permutation
 from src.math_library.monoid_elements_generator import MonoidElementsGenerator
 import src.math_library.positive_bt_permutations as positive_bt_permutations
 
+
 HEIGHT = 2
 WIDTH = 3
 
-csv_name = 'data.csv'
+csv_path = 'data.csv'
 
 monoid_elements = MonoidElementsGenerator(
     HEIGHT, WIDTH).generate_monoid_elements()
 
-with open(csv_name, 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow([
-        'leaves',
-        *['x' + str(i) for i in range(WIDTH)],
-        'bottom_permutation',
-        'top_permutation',
-        'permutation',
-        'orbits'
-    ])
-    for i in range(len(monoid_elements)):
-        v = np.array(monoid_elements[i][:])
-        v = np.insert(v, 0, 1, axis=0)
-        n = positive_bt_permutations.number_of_leaves(v)
-        q = Permutation(positive_bt_permutations.bottom_permutation(n))
-        p = Permutation(positive_bt_permutations.top_permutation(n, v))
-        r = positive_bt_permutations.whole_permutation(n, v)
-        writer.writerow([] + [v[i] for i in range(WIDTH)] + [q, p, r, len(r)])
+
+def create_dataset(monoid_elements: np.ndarray, width: int, csv_path: str):
+    with open(csv_path, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow([
+            'leaves',
+            *['x' + str(i) for i in range(width)],
+            'bottom_permutation',
+            'top_permutation',
+            'permutation',
+            'orbits'
+        ])
+        for i in range(len(monoid_elements)):
+            monoid_element = np.array(monoid_elements[i][:])
+            # monoid_element = np.insert(monoid_element, 0, 1, axis=0)
+            number_of_leaves = positive_bt_permutations.number_of_leaves(
+                monoid_element)
+            bottom_permutation = Permutation(
+                positive_bt_permutations.bottom_permutation(number_of_leaves))
+            top_permutation = Permutation(
+                positive_bt_permutations.top_permutation(number_of_leaves, monoid_element))
+            whole_permutation = positive_bt_permutations.whole_permutation(
+                number_of_leaves, monoid_element)
+            writer.writerow(
+                [number_of_leaves] +
+                [monoid_element[i] for i in range(width)] +
+                [bottom_permutation, top_permutation,
+                    whole_permutation, len(whole_permutation)]
+            )
+
+
+if __name__ == '__main__':
+    csv_path = 'data.csv'
+
+    HEIGHT = 3
+    WIDTH = 3
+
+    monoid_elements = MonoidElementsGenerator(
+        HEIGHT, WIDTH).generate_monoid_elements()
+
+    create_dataset(monoid_elements, WIDTH, csv_path)
