931de62aed55df216b6cb8312114fdfc44411fe1
Valeriano Aiello <104353469+valerianoaiello@users.noreply.github.com>
2022-12-03 18:55:01 +0100

    Update dataset_creation.py

--- src/dataset/dataset_creation.py
+++ src/dataset/dataset_creation.py
@@ -5,7 +5,17 @@ from sympy.combinatorics import Permutation
 from src.math_library.monoid_elements_generator import MonoidElementsGenerator
 import src.math_library.positive_bt_permutations as positive_bt_permutations
 
-
+"""
+This function creates a csv file with 5 columns: the 
+the first contains the number of leaves in the reduced tree diagram,
+then we have the vector (a_0, ..., a_width) representing the element of F_{3,+},
+then we have the bottom_permutation, the top_permutation, the Thompson permutation, finally the number of orbits.
+All permutations are expressed with the cycle notation. 
+The function create_dataset takes two inputs: a numpy array and a string. 
+The numpy array is produced with the method generate_monoid_elements (defined in the file monoid_elements_generator.py ), 
+which returns all the vectors in R^width whose components are between 0 and height.
+The string contains the path where the csv file will be saved.
+"""
 def create_dataset(monoid_elements: np.ndarray, csv_path: str):
     _, width = monoid_elements.shape
     with open(csv_path, 'w') as f:
