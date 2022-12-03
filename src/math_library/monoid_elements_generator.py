780857fa2998fafe7dff05ddd3d9e12f20a46fb4
Stefano Iovieno <stefano.iovieno@gmail.com>
2022-12-03 16:36:11 +0100

    Update comments in monoid_elements_generator.py

--- src/math_library/monoid_elements_generator.py
+++ src/math_library/monoid_elements_generator.py
@@ -2,14 +2,13 @@ import numpy as np
 
 """
 An element of F_{3,+} can be written uniquely as x_0^{a_0}\cdots x_n^{a_n} for some n, a_0, ..., a_{n-1} in N \cup {0} and 
-a_n in N, which we represent it as vector v=(a_0,a_1, ..., a_n).
+a_n in N, which we represent it as vector v = (a_0,a_1, ..., a_n).
 The method generate_monoid_elements returns all the vectors in R^width whose components are between 0 and height.
 The idea is the following: we first create a vector called admissible_exponents with entries from 0, to height in ascending order.
-Then we create a matrix "repeated_admissible_exponents" in M_{height + 1, width}(Z), whose columns are all equal to "admissible_exponents".
-Finally, we obtain all the desired vectors by selecting an entry in "repeated_admissible_exponents per column.
+Then we create a matrix "repeated_admissible_exponents" in M_{height, width}(Z), whose columns are all equal to "admissible_exponents".
+Finally, we obtain all the desired vectors by selecting an entry in "repeated_admissible_exponents" per column.
 
 The method random_generate_monoid_elements picks n random vectors in R^width whose components are between 0 and height.
-
 """
 class MonoidElementsGenerator:
     def __init__(self, height: int, width: int) -> None:
